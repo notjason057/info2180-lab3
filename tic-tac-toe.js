01f4848cf9375bb5669e8555a708b26c370574e4
Jason Matthews <jasonmatthews@Mac.home.lla.com>
2025-10-17 20:17:10 -0500

    error fixed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cells = document.querySelectorAll('#board div');
+  const statusDiv = document.getElementById('status');
   let turn = 'X';
   const gameState = Array(9).fill(null);
+
+
 
   const winningPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8], [2, 4, 6], [0, 4, 8], [1, 4, 7], [0, 3, 6]
