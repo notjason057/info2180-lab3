565705a7ae058334ee5ce70e281a4f8662c83a38
Jason Matthews <jasonmatthews@Mac.home.lla.com>
2025-10-17 20:10:40 -0500

    Winner code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   let turn = 'X';
   const gameState = Array(9).fill(null);
 
+  const winningPatterns = [
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7, 8]
+    [2, 5, 8]
+    [2, 4, 6]
+    [0, 4, 8]
+    [1, 4, 7]
+    [0, 3, 6]
+  ];
+
+  function verifyWinner(){
+    for (const combo of winningPatterns) {
+      const [a, b, c] = combo;
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        statusDiv.textContent = `Congratulations! ${gameState[a]} is the Winner!`;
+        statusDiv.classList.add('you-won');
+        return true;
+      }
+    }
+    return false;
+  }
+  
   cells.forEach((cell, index) => {
     cell.classList.add('square');
 
